170dd12818830f0145d9467a0d63a4e54ecbd6a2
Kittipit <kpinthawornrak@gmail.com>
2023-09-25 02:02:26 +0700

    Add some data to db and link customer page

--- app_general/models.py
+++ app_general/models.py
@@ -4,3 +4,13 @@ from django.db import models
 class Reservation(models.Model):
     total_price = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Foods(models.Model):
+    foods_name = models.CharField(max_length=255)
+    price = models.IntegerField()
+
+class Orders(models.Model):
+    foods = models.ForeignKey('app_general.Foods', on_delete=models.SET_NULL, null=True)
+    queue = models.ForeignKey('app_general.Reservation', on_delete=models.SET_NULL, null=True)
+    amount = models.IntegerField()
+    price = models.IntegerField()
--- app_general/views.py
+++ app_general/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'app_general/home.html')
+
+def customer(request):
+    return render(request, 'app_general/customer_page1.html')
