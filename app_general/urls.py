718bf7103baae8c03b6f3bca5901da4f8b400213
Kittipit <kpinthawornrak@gmail.com>
2023-09-27 23:19:13 +0700

    link css with html file for testing and add some view to prepare linking web page

--- app_general/urls.py
+++ app_general/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('customer/', views.customer, name='customer')
+    path('customer/', views.shop, name='shop')
 ]
