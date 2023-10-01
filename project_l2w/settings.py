8954313153d29fc09768c5176fc138a53555c813
tonnam250 <tonnam250@gmail.com>
2023-10-02 00:35:59 +0700

    install tailwind again but through django pip

--- project_l2w/settings.py
+++ project_l2w/settings.py
@@ -38,6 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app_general.apps.AppGeneralConfig',
+    'tailwind',
+    'theme'
+]
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1"
 ]
 
 MIDDLEWARE = [
